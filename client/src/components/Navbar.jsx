7cd3f481f1851f8c15c23624634eec9c57f382e7
Yug <03yugagarwal@gmail.com>
2023-11-06 11:27:52 +0530

    create post

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+ import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar_container">
+        <h1 className="navbar_header">MindfulHub</h1>
+        <ul className="navbar_ul">
+            <li><a href="#">BLOGS</a></li>
+            <li><a href="#">RESOURCES</a></li>
+            <li><a href="#">CHAT</a></li>
+            <li><a href="#">LOGIN</a></li>
+
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
