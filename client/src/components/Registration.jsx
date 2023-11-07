a24a53ed829e79e76033c246911d2366ea1f292f
SwarabRaul <swarab.raul@gmail.com>
2023-11-07 09:39:03 +0530

    feat(registration.jsx & registration.css): added: Registration.jsx & Registration.css
    Tested and working, but Registration.css is commented out.

---
+++ client/src/components/Registration.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+// import './Registration.css'
+
+const Registration = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async () => {
+        // Send a POST request to your server to create a new user
+        const response = await fetch('http://localhost:8080/user/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, email, password }),
+        });
+
+        if (response.status === 200) {
+            // User registered successfully, you can show a success message or redirect to the login page
+            // Example: window.location.href = '/login';
+        } else {
+            // Handle registration error
+            console.error('Registration failed');
+        }
+    };
+
+    return (
+        <div>
+            <h2>Register</h2>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="email"
+                placeholder="Email Id"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleRegister}>Register</button>
+        </div>
+    );
+};
+
+export default Registration;
