111410c151e466baa773cc10418fa6127018e6ce
vineetha mathews <vineethamathews1234@gmail.com>
2023-11-08 00:55:12 +0530

    Login-restyling2

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import './Login.css'
+import './Login.css'
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -39,22 +39,26 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+        <div class="login-container">
+        <h2>MindfulHub</h2>
+        <form class="login-form" action="login.php" method="post">
+            <label for="username">Username</label>
+            <input type="text" 
+             value={username} 
+             required placeholder='Enter username'
+             onChange={(e) => setUsername(e.target.value)}
+             />
+
+            <label for="password">Password</label>
+            <input type="password" 
+            value={password} 
+            required placeholder='Enter Password'
+            onChange={(e) => setPassword(e.target.value)}
             />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin}>Login</button>
-        </div>
+
+            <button class="login" onClick={handleLogin}>Login</button>
+        </form>
+    </div>
     );
 };
 
