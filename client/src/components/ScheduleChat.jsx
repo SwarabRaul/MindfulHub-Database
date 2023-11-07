af6dc93180fb651d079e45f1cf9a87d3f480ddce
SwarabRaul <swarab.raul@gmail.com>
2023-11-07 09:40:29 +0530

    feat(schedule.jsx & schedule.css): added: Schedule.jsx & Schedule.css
    Backend is not implemented. But Frontend is done.

---
+++ client/src/components/ScheduleChat.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './ScheduleChat.css';
+
+const ScheduleChat = () => {
+    const [user, setUser] = useState('');
+    const [expert, setExpert] = useState('');
+    const [scheduledDateTime, setScheduledDateTime] = useState('');
+    const [message, setMessage] = useState('');
+    const [isAnonymous, setIsAnonymous] = useState(false);
+
+    const handleScheduleChat = async (e) => {
+        e.preventDefault();
+
+        const chatData = {
+            user,
+            expert,
+            scheduledDateTime,
+            message,
+            isAnonymous,
+        };
+
+        setUser('');
+        setExpert('');
+        setScheduledDateTime('');
+        setMessage('');
+        setIsAnonymous(false);
+    };
+
+    return (
+        <div className="schedulechat">
+            <div className="schedulechat_container">
+                <div className="schedulechat_left">
+                    <h1>Schedule a Chat</h1>
+                    <h4>
+                        Schedule a chat with a psychology expert to get help, help someone, or ask questions.
+                    </h4>
+                </div>
+                <div className="schedulechat_right">
+                    <form onSubmit={handleScheduleChat}>
+                        <input
+                            type="text"
+                            placeholder="User"
+                            value={user}
+                            onChange={(e) => setUser(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Expert"
+                            value={expert}
+                            onChange={(e) => setExpert(e.target.value)}
+                        />
+                        <input
+                            type="datetime-local"
+                            placeholder="Scheduled Date and Time"
+                            value={scheduledDateTime}
+                            onChange={(e) => setScheduledDateTime(e.target.value)}
+                        />
+                        <textarea
+                            placeholder="Message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                        <div className="schedulechat_right_checkbox">
+                            <input
+                                type="checkbox"
+                                id="anonymous"
+                                checked={isAnonymous}
+                                onChange={() => setIsAnonymous(!isAnonymous)}
+                            />
+                            <label htmlFor="anonymous">Post Anonymously</label>
+                        </div>
+                        <button type="submit">Schedule Chat</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ScheduleChat;
