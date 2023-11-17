14effa5705b18b3d104701a00ca5a72bd6c4264e
Swarab Raul <swarab.raul@gmail.com>
2023-11-18 00:01:16 +0530

    chore(Navbar.jsx): Cleaned up code, and imported Navbar.css.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,17 @@
-import { useEffect } from "react";
-import "./Navbar.css";
-
+// Navbar.js
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import '../styles/Navbar.css';
 
 const Navbar = () => {
-
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
 
   const loginButton = localStorage.getItem("token") ? (
     <li>
-      <button
-        onClick={() => {
-          localStorage.removeItem("token");
-          window.location.reload();
-        }}
-      >
-        LOGOUT
-      </button>
+      <button onClick={handleLogout}>LOGOUT</button>
     </li>
   ) : (
     <li>
@@ -25,9 +21,14 @@ const Navbar = () => {
 
   return (
     <div className="navbar_container">
-      <h1 className="navbar_header">
-        <Link to="/">MindfulHub</Link>
-      </h1>
+      <div className="navbar_logo">
+        <Link to="/">
+          <img src="MindfulHub_LatestLogo.png" alt="MindfulHub Logo" />
+        </Link>
+        <h1 className="navbar_header">
+          <Link to="/" className="navbar_link">MindfulHub</Link>
+        </h1>
+      </div>
       <ul className="navbar_ul">
         <li>
           <NavLink to="/blog">BLOGS</NavLink>
